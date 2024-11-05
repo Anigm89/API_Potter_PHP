ef0e240c077853b62a72f85fc162fffb1841157f
Aniana <anigm89@gmail.com>
2024-11-05 14:40:43 +0100

    primeros datos sin css

--- index.php
+++ index.php
@@ -10,8 +10,19 @@ require_once 'functions.php';
         <h1>Personajes de Harry Potter</h1> 
         <?php
             $data = get_data($url);
-            var_dump($data);
-            ?>
+            //var_dump($data);
+        ?>
+        <ul>
+        <?php foreach($data as $character) : ?>
+            <li>
+                <p>Nombre: <?= $character['fullName'] ?> </p>
+                <p>Alias:  <?= $character['nickname'] ?> </p>
+                <p>Casa:  <?= $character['hogwartsHouse'] ?> </p>
+                <p>fecha de nacimiento:  <?= $character['birthdate'] ?> </p>
+                <img src=" <?= $character['image'] ?>" alt=" <?= $character['nickname'] ?>">
+            </li>
+        <?php endforeach ?>
+        </ul>
     </section>
 </body>
 </html>
