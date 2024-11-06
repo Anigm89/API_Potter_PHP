b7deb232abfc9960ec29dc5db994854e74f904f1
Aniana <anigm89@gmail.com>
2024-11-06 18:33:34 +0100

    con classes -  OOP

--- functions.php
+++ functions.php
@@ -1,18 +1,19 @@
 <?php
 declare(strict_types=1);
-$url = 'https://potterapi-fedeperin.vercel.app/es/characters';
+const API_URL = 'https://potterapi-fedeperin.vercel.app/es/characters';
 
+/* obtener datos api sin classes
 function get_data( string $url) : array
 {
     $result = file_get_contents($url);
     $data = json_decode($result, true);
     return $data;
 }
+*/
 
 function render_template( string $template, array $data = [])
 {
     extract($data);
-   // var_dump($data);
     require "templates/$template.php";
 }
 
--- index.php
+++ index.php
@@ -1,28 +1,12 @@
 <?php
 require_once 'functions.php';
+require_once 'classes/characters.php';
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <?php render_template('head'); ?>
 <body>
-    <?php /* render_template('section'); */ ?>
-    <section>
-        <h1>Personajes de Harry Potter</h1> 
-        <?php
-            $data = get_data($url);
-            //var_dump($data);
-        ?>
-        <ul>
-        <?php foreach($data as $character) : ?>
-            <li>
-                <p class="name"> <?= $character['fullName'] ?> </p>
-                <img src=" <?= $character['image'] ?>" alt=" <?= $character['nickname'] ?>">
-                <p>Alias: <span>  <?= $character['nickname'] ?> </span> </p>
-                <p><span> <?= $character['hogwartsHouse'] ?> </span> </p>
-                <p> <span> <?= $character['birthdate'] ?> </span> </p>
-            </li>
-        <?php endforeach ?>
-        </ul>
-    </section>
+    <?php  render_template('section');  ?>
 </body>
 </html>
